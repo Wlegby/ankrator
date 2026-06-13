691b71cebeabbe32c2ce30ded68d2b76ae7ebee9
Wlegby <gigamullman@gmail.com>
2026-06-13 18:15:03 +0200

    idk

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,226 @@
-use anker::notes::Note;
+use anker::markdown::markdown_to_anki_with_typst;
+use anker::notes::{format_cloze, Note};
 use anker::AnkiClient;
 use std::collections::HashMap;
-use std::fs;
+use std::fs::{self, File};
+use std::io::BufWriter;
+use std::time::SystemTime;
 
 mod parse_file;
 use parse_file::parse_file;
 
+use crate::parse_file::{Parts, Types};
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // anker::launch_anki()?;
+    let client = AnkiClient::default();
 
-    let content = fs::read_to_string("input").unwrap();
+    let path = "input".to_string();
+    let content = fs::read_to_string(&path)?;
+    match handle_file(&content, path, &client).await {
+        Ok(_) => {}
+        Err(e) => eprintln!("Failed to handle file:\n{}", e),
+    }
 
-    let parsed_file = parse_file(content.as_str())?;
+    Ok(())
+}
 
-    dbg!(parsed_file);
+#[derive(Debug, Clone, Copy)]
+enum CardType<'a> {
+    Cloze { text: &'a str },
+    Basic { front: &'a str, back: &'a str },
+}
+impl<'a> Default for CardType<'a> {
+    fn default() -> Self {
+        Self::Cloze { text: "" }
+    }
+}
 
+async fn handle_file<'a>(
+    content: &'a str,
+    path: String,
+    client: &AnkiClient,
+) -> Result<(), Box<dyn std::error::Error>> {
+    match file_changed(&path) {
+        Ok(changed) => {
+            if !changed {
+                return Ok(());
+            }
+        }
+        Err(i) => eprintln!(
+            "Failed to check if file changed ({}), going to parse it anyways",
+            i
+        ),
+    }
+
+    let mut parsed_file = parse_file(content)?;
+    handle_parts(&mut parsed_file, path, client).await?;
+
+    Ok(())
+}
+
+fn file_changed<'a>(path: &str) -> Result<bool, Box<dyn std::error::Error>> {
+    if let Some(mut dir) = dirs::cache_dir() {
+        dir = dir.join("ankrator");
+        fs::create_dir_all(&dir)?;
+        dir = dir.join("cache.json");
+
+        if !dir.exists() {
+            fs::write(&dir, "{}")?;
+        }
+
+        let cache = fs::read_to_string(&dir)?;
+
+        let mut map: HashMap<String, SystemTime> = serde_json::from_str(&cache)?;
+
+        let last_time = match map.get_mut(path) {
+            Some(last_time) => {
+                let _return = last_time.clone();
+                *last_time = SystemTime::now();
+                _return
+            }
+            None => {
+                map.insert(path.to_string(), SystemTime::now());
+                SystemTime::now()
+            }
+        };
+
+        let file = File::create(&dir)?;
+        let writer = BufWriter::new(file);
+        serde_json::to_writer_pretty(writer, &map)?;
+
+        let metadata = fs::metadata(path)?;
+        let modified_time: SystemTime = metadata.modified().unwrap();
+
+        Ok(modified_time < last_time)
+    } else {
+        eprint!("Failed to get cache dir");
+        Ok(true)
+    }
+}
+
+async fn handle_parts<'a>(
+    parsed_file: &mut Vec<Parts<'a>>,
+    path: String,
+    client: &AnkiClient,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut deck = "Default";
+    let mut tags: Vec<&str> = Vec::new();
+    let mut card_type = CardType::Cloze { text: "" };
+
+    let mut new_file = String::new();
+
+    for part in parsed_file.iter_mut() {
+        match part {
+            Parts::DeckName(name) => {
+                new_file.push_str(&format!("@deck {}\n", name));
+                deck = name;
+            }
+            Parts::Tags(t) => {
+                new_file.push_str(&format!("@tags "));
+                for (i, tag) in t.iter().enumerate() {
+                    new_file.push_str(&format!("{}", tag));
+                    if i < t.len() - 1 {
+                        new_file.push_str(&format!(", "));
+                    } else {
+                        new_file.push_str("\n\n");
+                    }
+                }
+
+                tags = t.clone();
+            }
+            Parts::CardType(ctype) => match ctype {
+                Types::Basic => {
+                    new_file.push_str(&format!("# Basic\n"));
+                    card_type = CardType::Basic {
+                        front: "",
+                        back: "",
+                    }
+                }
+                Types::Cloze => {
+                    new_file.push_str(&format!("# Cloze\n"));
+                    card_type = CardType::Cloze { text: "" }
+                }
+                Types::Unknown => return Err("Failed to parse card type".into()),
+            },
+            Parts::Front(cfront) => match &mut card_type {
+                CardType::Basic { front, back: _ } => {
+                    *front = cfront;
+                    new_file.push_str(&format!("Front: {}\n", front));
+                }
+                CardType::Cloze { text } => {
+                    return Err(format!("Expected basic style card ({})", text).into());
+                }
+            },
+            Parts::Back(cback) => match &mut card_type {
+                CardType::Basic { front: _, back } => {
+                    *back = cback;
+                    new_file.push_str(&format!("Back: {}\n", back));
+                }
+                CardType::Cloze { text } => {
+                    return Err(format!("Expected basic style card ({})", text).into())
+                }
+            },
+            Parts::ClozeLine(line) => match &mut card_type {
+                CardType::Basic { front, back } => {
+                    return Err(format!("Expected basic style card ({} & {})", front, back).into())
+                }
+                CardType::Cloze { text } => {
+                    *text = line;
+                    new_file.push_str(&format!("Cloze: {}\n", text));
+                }
+            },
+            Parts::CardEnd(cid) => {
+                let id = match cid {
+                    Some(i) => {
+                        new_file.push_str(&format!("---NoteID:{}\n", i));
+                        let parsed = match i.trim().parse::<i64>() {
+                            Ok(i) => i,
+                            Err(e) => return Err(e.into()),
+                        };
+                        Some(parsed)
+                    }
+                    None => None,
+                };
+
+                let mut fields = HashMap::new();
+                let model_name = match card_type {
+                    CardType::Cloze { text } => {
+                        let _ = fields.insert(
+                            "Text".to_string(),
+                            markdown_to_anki_with_typst(&format_cloze(text)),
+                        );
+                        "Cloze".to_string()
+                    }
+                    CardType::Basic { front, back } => {
+                        fields.insert("Front".to_string(), markdown_to_anki_with_typst(front));
+                        fields.insert("Back".to_string(), markdown_to_anki_with_typst(back));
+                        "Basic".to_string()
+                    }
+                };
+
+                if let Some(id) = id {
+                    client.notes().update_note_fields(id, &fields).await?;
+                    continue;
+                }
+
+                let note = Note {
+                    deck_name: deck.to_string(),
+                    model_name,
+                    fields,
+                    tags: tags.iter().map(|t| t.to_string()).collect(),
+                };
+
+                let id = client.notes().add_note(&note).await?;
+                new_file.push_str(&format!("---NoteID:{}\n", id));
+                card_type = CardType::default();
+            }
+            Parts::Comment(c) => {
+                new_file.push_str(&format!("//{}\n", c));
+            }
+        }
+    }
+
+    fs::write(path, new_file)?;
     Ok(())
 }
--- src/parse_file.rs
+++ src/parse_file.rs
@@ -1,6 +1,6 @@
-use rparse::{literal, split_at, take_until, Parser};
+use rparse::{literal, split_at, take_until, take_while, Parser};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Parts<'a> {
     DeckName(&'a str),
     Tags(Vec<&'a str>),
@@ -8,11 +8,11 @@ pub enum Parts<'a> {
     Front(&'a str),
     Back(&'a str),
     ClozeLine(&'a str),
-    CardEnd,
+    CardEnd(Option<&'a str>),
     Comment(&'a str),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum Types {
     Basic,
     Cloze,
@@ -34,7 +34,14 @@ pub fn parse_tags<'a>() -> impl Parser<'a, Parts<'a>> {
         })
 }
 pub fn parse_card_end<'a>() -> impl Parser<'a, Parts<'a>> {
-    literal("---").map(|_| Parts::CardEnd)
+    literal("---")
+        .and((literal("NoteID:").and(take_until(|c| c == '\n' || c == '\r'))).opt())
+        .map(|(_, id)| {
+            Parts::CardEnd(match id {
+                Some((_, id)) => Some(id),
+                None => None,
+            })
+        })
 }
 
 pub fn parse_card_type<'a>() -> impl Parser<'a, Parts<'a>> {
@@ -52,13 +59,13 @@ pub fn parse_card_type<'a>() -> impl Parser<'a, Parts<'a>> {
 pub fn parse_front<'a>() -> impl Parser<'a, Parts<'a>> {
     literal("Front: ")
         .and(split_at("Back:"))
-        .map(|(_, text)| Parts::Front(text))
+        .map(|(_, text)| Parts::Front(text.trim()))
 }
 
 pub fn parse_back<'a>() -> impl Parser<'a, Parts<'a>> {
     literal("Back: ")
         .and(split_at("---"))
-        .map(|(_, text)| Parts::Back(text))
+        .map(|(_, text)| Parts::Back(text.trim()))
 }
 
 pub fn parse_cloze<'a>() -> impl Parser<'a, Parts<'a>> {
@@ -69,7 +76,7 @@ pub fn parse_cloze<'a>() -> impl Parser<'a, Parts<'a>> {
 pub fn parse_comment<'a>() -> impl Parser<'a, Parts<'a>> {
     literal("//")
         .and(take_until(|c| c == '\n' || c == '\r'))
-        .map(|(_, comment)| Parts::Comment(comment))
+        .map(|(_, c)| Parts::Comment(c))
 }
 
 pub fn parse_file<'a>(mut input: &'a str) -> Result<Vec<Parts<'a>>, &'a str> {
@@ -84,14 +91,13 @@ pub fn parse_file<'a>(mut input: &'a str) -> Result<Vec<Parts<'a>>, &'a str> {
         .or(parse_card_end())
         .or(parse_comment());
 
-    let whitespace = literal("\n").or(literal("\r")).or(literal("\r\n"));
+    let whitespace = take_while(|c| c == '\n' || c == '\r');
 
     while !input.is_empty() {
         match whitespace.parse(input) {
             Ok(_) => input = input.trim(),
             Err(_) => {}
         }
-
         match parts_parser.parse(input) {
             Ok((rest, part)) => {
                 parts.push(part);
